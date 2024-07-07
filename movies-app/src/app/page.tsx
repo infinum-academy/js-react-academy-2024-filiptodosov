f5b9f280dfda9fc496c898afe577f65707962d34
Filip Todosov <filiptodosov@Filips-MacBook-Pro.local>
2024-07-07 22:53:44 +0200

    Add average rating feature.

--- movies-app/src/app/page.tsx
+++ movies-app/src/app/page.tsx
@@ -13,31 +13,41 @@ let mockReviewItems: Array<IReviewItem> = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nulla diam, maximus eget vulputate id, tristique id odio. Aliquam vehicula dui sit amet euismod eleifend.",
     rating: 1,
   },
-  // {
-  //   reviewText:
-  //     "Donec pulvinar, quam quis bibendum congue, mi justo sagittis eros, sed finibus augue augue vel enim. Aliquam sodales, tellus eget lobortis laoreet, nulla eros fermentum tellus, faucibus congue erat justo nec magna.",
-  //   rating: 2,
-  // },
-  // {
-  //   reviewText:
-  //     "Duis quis iaculis quam. Duis tempus ornare suscipit. Morbi id erat nec est placerat scelerisque suscipit ut justo.",
-  //   rating: 3,
-  // },
-  // {
-  //   reviewText:
-  //     "Donec vitae orci auctor, interdum libero sit amet, viverra augue. Pellentesque purus risus, malesuada eleifend malesuada eu, tempus nec ante.",
-  //   rating: 4,
-  // },
-  // {
-  //   reviewText:
-  //     "Sed vel nulla dignissim, facilisis lacus in, tincidunt augue. Vestibulum sagittis nibh nec erat egestas, vitae mattis velit euismod. Fusce a ligula id sem semper pulvinar.",
-  //   rating: 5,
-  // },
+  {
+    reviewText:
+      "Donec pulvinar, quam quis bibendum congue, mi justo sagittis eros, sed finibus augue augue vel enim. Aliquam sodales, tellus eget lobortis laoreet, nulla eros fermentum tellus, faucibus congue erat justo nec magna.",
+    rating: 2,
+  },
+  {
+    reviewText:
+      "Duis quis iaculis quam. Duis tempus ornare suscipit. Morbi id erat nec est placerat scelerisque suscipit ut justo.",
+    rating: 3,
+  },
+  {
+    reviewText:
+      "Donec vitae orci auctor, interdum libero sit amet, viverra augue. Pellentesque purus risus, malesuada eleifend malesuada eu, tempus nec ante.",
+    rating: 4,
+  },
+  {
+    reviewText:
+      "Sed vel nulla dignissim, facilisis lacus in, tincidunt augue. Vestibulum sagittis nibh nec erat egestas, vitae mattis velit euismod. Fusce a ligula id sem semper pulvinar.",
+    rating: 5,
+  },
 ];
 
 let reviewItemsLocal: Array<IReviewItem> = JSON.parse(
   localStorage.getItem("reviewItems")
 );
+
+const calculateAverageRating = (reviewItems: Array<IReviewItem>) => {
+  let sumOfReviews = 0;
+
+  reviewItems.map((reviewItem) => {
+    sumOfReviews += reviewItem.rating;
+  });
+
+  return Math.round((sumOfReviews / reviewItems.length) * 10) / 10;
+};
 
 export default function Home() {
   if (reviewItemsLocal == undefined) {
@@ -45,20 +55,23 @@ export default function Home() {
   }
 
   let [reviewItems, setReviewItems] = useState(reviewItemsLocal);
+  let [averageRating, setAverageRating] = useState(
+    calculateAverageRating(reviewItems)
+  );
 
   const addShowReview = () => {
     const newReviewItem: IReviewItem = {
       reviewText: document.getElementById("reviewDescription").value,
-      rating: document.getElementById("reviewRating").value,
+      rating: parseInt(document.getElementById("reviewRating").value, 10),
     };
-
-    console.log(newReviewItem);
 
     if (newReviewItem.rating && newReviewItem.reviewText) {
       localStorage.setItem(
         "reviewItems",
         JSON.stringify([...reviewItems, newReviewItem])
       );
+      setAverageRating(calculateAverageRating([...reviewItems, newReviewItem]));
+
       setReviewItems([...reviewItems, newReviewItem]);
       document.getElementById("reviewDescription").value = "";
       document.getElementById("reviewRating").value = "";
@@ -78,6 +91,7 @@ export default function Home() {
           description:
             "Comedy series following the exploits of Det. Jake Peralta and his diverse, lovable colleagues as they police the NYPD's 99th Precinct.",
           reviewList: reviewItems,
+          averageRating: averageRating,
         }}
       ></ShowDetails>
       <ShowReviewSection
