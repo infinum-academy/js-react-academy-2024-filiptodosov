ec6a854929757f36a6b132d84d1af4496b7cd165
Filip Todosov <todosov.filip@icloud.com>
2024-07-21 15:17:07 +0200

    remove extra state for keeping show reviews

--- movies-app/src/components/features/shows/ReviewForm/ReviewForm.tsx
+++ movies-app/src/components/features/shows/ReviewForm/ReviewForm.tsx
@@ -27,72 +27,72 @@ import { useParams } from "next/navigation";
 import { reviewsMutator } from "@/fetchers/mutators";
 import { useSWRConfig } from "swr";
 
- 
-interface IReviewFormProps {
-  addShowReview: (reviewItem: IReviewItem) => void,
-}
-
-export const ReviewForm = ({ addShowReview }: IReviewFormProps) => {
-  const { mutate } = useSWRConfig()
+export const ReviewForm = () => {
+  const { mutate } = useSWRConfig();
 
   const params = useParams();
   const showId = params.id;
 
-  const { register, handleSubmit, formState: { isSubmitting, isDirty, isValid }, reset } = useForm<IReviewItem>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, isValid },
+    reset,
+  } = useForm<IReviewItem>();
 
   const toast = useToast();
-  
-const {trigger} = useSWRMutation(swrKeys.reviews, reviewsMutator, {
-  onSuccess: ()=>{
-    reset();
-    toast({
-      title: 'Yay!',
-      description: "You have submitted your review.",
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    });
 
-    mutate(swrKeys.showReviews(showId as string));
-  }
+  const { trigger } = useSWRMutation(swrKeys.reviews, reviewsMutator, {
+    onSuccess: () => {
+      reset();
+      toast({
+        title: "Yay!",
+        description: "You have submitted your review.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
 
-});
+      mutate(swrKeys.showReviews(showId as string));
+    },
+  });
 
+  const onSubmit = async (data: IReviewItem) => {
+    const newData: IReviewItem = {
+      comment: data.comment,
+      rating: parseFloat(data.rating),
+      show_id: data.show_id,
+    };
 
-const onSubmit = async (data:IReviewItem) => {
-  const newData:IReviewItem = {
-    comment: data.comment,
-    rating: parseFloat(data.rating),
-    show_id: data.show_id
-  }
+    await trigger(newData);
+  };
 
-  await trigger(newData);
-}
-
-
-return (
-  <chakra.form  onSubmit={handleSubmit(onSubmit)}>
-    <Heading size="md">Add a review</Heading>
-    <FormControl isRequired={true}>
-      <FormLabel>Review comment</FormLabel>
-      <Textarea  {...register('comment')} placeholder="Your review comment here..." id="reviewDescription" />
-  </FormControl>
-    <FormControl isRequired={true}>
-      <FormLabel>Rating:</FormLabel>
+  return (
+    <chakra.form onSubmit={handleSubmit(onSubmit)}>
+      <Heading size="md">Add a review</Heading>
+      <FormControl isRequired={true}>
+        <FormLabel>Review comment</FormLabel>
+        <Textarea
+          {...register("comment")}
+          placeholder="Your review comment here..."
+          id="reviewDescription"
+        />
+      </FormControl>
+      <FormControl isRequired={true}>
+        <FormLabel>Rating:</FormLabel>
         <NumberInput step={1} min={0} max={5} defaultValue={5}>
-          <NumberInputField   {...register("rating")} />
+          <NumberInputField {...register("rating")} />
           <NumberInputStepper>
             <NumberIncrementStepper />
             <NumberDecrementStepper />
           </NumberInputStepper>
         </NumberInput>
-      <input type="hidden" value={showId} {...register("show_id")} />
-    </FormControl>
+        <input type="hidden" value={showId} {...register("show_id")} />
+      </FormControl>
 
-    <Button marginTop={2} marginBottom={2} colorScheme="blue" type="submit">
-      Submit review
-    </Button>
-
-  </chakra.form>
-);
-}
+      <Button marginTop={2} marginBottom={2} colorScheme="blue" type="submit">
+        Submit review
+      </Button>
+    </chakra.form>
+  );
+};
--- movies-app/src/components/features/shows/ShowReviewSection/ShowReviewSection.tsx
+++ movies-app/src/components/features/shows/ShowReviewSection/ShowReviewSection.tsx
@@ -1,35 +1,23 @@
-import {
-    Flex,
-    useToast,
-    Card,
-    CardBody,
-  } from "@chakra-ui/react";
-  import { ReviewList } from "../../review/ReviewList/ReviewList";
-  import { ReviewForm } from "../ReviewForm/ReviewForm";
-  import { IReviewList } from "../../../../typings/ReviewList.type";
-  import { IReviewItem } from "@/typings/ReviewItem.type";
-  
+import { Flex, useToast, Card, CardBody } from "@chakra-ui/react";
+import { ReviewList } from "../../review/ReviewList/ReviewList";
+import { ReviewForm } from "../ReviewForm/ReviewForm";
+import { IReviewList } from "../../../../typings/ReviewList.type";
+import { IReviewItem } from "@/typings/ReviewItem.type";
 
-    export interface IShowReviewSectionProps {
-      reviewList: IReviewList,
-      addShowReview: (newReviewItem: IReviewItem) => void,
-      deleteShowReview: (key: number) => void,
-  }
+export interface IShowReviewSectionProps {
+  reviewList: IReviewList;
+  addShowReview: (newReviewItem: IReviewItem) => void;
+  deleteShowReview: (key: number) => void;
+}
 
-
-  export const ShowReviewSection = ({
-    reviewList,
-    addShowReview,
-    deleteShowReview,
-  }: IShowReviewSectionProps) => {
-    return (
-        <Card width="600px" marginTop={10}>
-          <CardBody>
-            <ReviewForm addShowReview={addShowReview} />
-            <hr />
-            <ReviewList reviewList={reviewList} deleteShowReview={deleteShowReview}/>
-          </CardBody>
-        </Card>
-    );
-  };
-  
+export const ShowReviewSection = ({ reviewList }: IShowReviewSectionProps) => {
+  return (
+    <Card width="600px" marginTop={10}>
+      <CardBody>
+        <ReviewForm />
+        <hr />
+        <ReviewList reviewList={reviewList} />
+      </CardBody>
+    </Card>
+  );
+};
